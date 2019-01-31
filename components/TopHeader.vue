9d7c5ae3eed4eafe21b60d89384b7af456c7b07f
Amir Haghighati <haghighati.amir@gmail.com>
2019-01-30 23:46:56 -0500

    update top header

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
-    <v-toolbar fixed app color="primary" dark id="toolbar">
-      <v-btn :to="'/'" nuxt icon class="user-button" style="margin-left: 20px">
-        <v-avatar
-          :tile="true"
-          :size="50">
-          <img src="logo.png" alt="بهترین وب‌اپلیکیشن فارغ‌التحصیلی">
-        </v-avatar>
-      </v-btn>
-      <v-toolbar-title v-text="title" class="title iransharp"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat v-for="item in items" :class="item.color" style="border-radius: 0px" :to="item.to" exact :key="item.title">{{item.title}}</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  </div>
+  <v-toolbar fixed app color="primary" dark>
+    <v-btn :to="'/'" nuxt icon class="user-button" style="margin-left: 20px">
+      <v-avatar
+        :tile="true"
+        :size="50">
+        <img src="logo.png" alt="بهترین وب‌اپلیکیشن فارغ‌التحصیلی">
+      </v-avatar>
+    </v-btn>
+    <v-toolbar-title class="title iransharp">فارغ‌التحصیلی ۹۳</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <!--<v-toolbar-items class="hidden-sm-and-down">-->
+      <!--<v-btn flat v-for="item in items" :class="item.color" style="border-radius: 0px" :to="item.to" exact :key="item.title">{{item.title}}</v-btn>-->
+    <!--</v-toolbar-items>-->
+  </v-toolbar>
 </template>
 
 <script>
@@ -22,18 +20,13 @@
     name: 'TopHeader',
     data () {
       return {
-        clipped: false,
-        drawer: false,
-        fixed: false,
-        miniVariant: false,
-        items: [
+        // items: [
           // { icon: 'mdi-message-text', title: 'آمار', to: '/visualization'},
           // { icon: 'mdi-alert-octagon', title: 'اطلاع‌رسانی', to: '/news'},
           // { icon: 'mdi-account', title: 'پروفایل', to: '/profile'},
 //          { icon: 'mdi-account-plus', title: 'ثبت‌نام جشن', to: '/register',color:'green darken-1'},
 //           { icon: 'mdi-exit-to-app', title: 'خروج', to: '/logout'},
-        ],
-        title: 'فارغ‌التحصیلی ۹۳',
+//         ]
       }
     },
   }
@@ -42,6 +35,7 @@
 <style scoped>
   .title {
     overflow: visible;
+    font-family: Iran, sans-serif !important;
   }
   .user-avatar {
     width: 45px;
